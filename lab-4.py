63c3c3fa663783bbede75037c68fbb7be117364f
Alexander Kartavenko <askemailbox@gmail.com>
2021-10-08 15:38:28 +0300

    l4 fixed

--- lab-4.py
+++ lab-4.py
@@ -1,18 +1,27 @@
+#!/usr/bin/python3
 import boto3
+source_ddb = boto3.resource('dynamodb', 'us-east-1')
+dest_ddb = boto3.client('dynamodb', 'us-west-2')
 
-def syncTables(event, context):
-    source_ddb = boto3.client('dynamodb', 'us-east-1')
-    destination_ddb = boto3.client('dynamodb', 'us-west-2')
-    sync_ddb_table(source_ddb, destination_ddb)
+def sync(source_ddb, dest_ddb):
+    table = source_ddb.Table("CodeGuru-MusicCollection")
+    scan_kwargs = {
+        'ProjectionExpression': "Artist, SongTitle"
+    }
+    done = False
+    start_key = None
+    while not done:
+        if start_key:
+            scan_kwargs['ExclusiveStartKey'] = start_key
+        response = table.scan(**scan_kwargs)
+        for item in response['Items']:
+            newItem = { 'Artist': {}, 'SongTitle': {} }
+            newItem['Artist']['S'] = item['Artist']
+            newItem['SongTitle']['S'] = item['SongTitle']
+            dest_ddb.put_item(TableName="CodeGuru-MusicCollection", Item=newItem)
+            print(item)
+        start_key = response.get('LastEvaluatedKey', None)
+        done = start_key is None
 
-# Scan returns paginated results, so only partial data will be copied
-def sync_ddb_table(source_ddb, destination_ddb):
-    response = source_ddb.scan(
-        TableName="<FMI1>"
-    )
 
-    for item in response['Items']:
-        destination_ddb.put_item(
-            TableName="<FMI2>",
-            Item=item
-        )
+sync(source_ddb, dest_ddb)
